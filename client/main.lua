d40481546dca14042d36c509f46c48966b899e85
Manason <clanerp@gmail.com>
2023-11-09 08:04:05 -0800

    fix(client): can now run out of oxygen & don't need to take off suit to re-fill

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,20 @@
 local currentGear = {
     mask = 0,
     tank = 0,
-    oxygen = 0,
     enabled = false
 }
 
 local oxygenLevel = 0
+
+local function enableScuba()
+    SetEnableScuba(cache.ped, true)
+    SetPedMaxTimeUnderwater(cache.ped, 2000.00)
+end
+
+local function disableScuba()
+    SetEnableScuba(cache.ped, false)
+    SetPedMaxTimeUnderwater(cache.ped, 1.00)
+end
 
 lib.callback.register('qbx_divegear:client:fillTank', function()
     if oxygenLevel ~= 0 then
@@ -15,6 +24,9 @@ lib.callback.register('qbx_divegear:client:fillTank', function()
 
     oxygenLevel = Config.OxygenLevel
     exports.qbx_core:Notify(Lang:t("success.tube_filled"), 'success')
+    if currentGear.enabled then
+        enableScuba()
+    end
     return true
 end)
 
@@ -30,6 +42,21 @@ local function deleteGear()
         DeleteEntity(currentGear.tank)
 		currentGear.tank = 0
 	end
+end
+
+local function attachGear()
+    local maskModel = `p_d_scuba_mask_s`
+    local tankModel = `p_s_scuba_tank_s`
+    lib.requestModel(maskModel)
+    lib.requestModel(tankModel)
+
+    currentGear.tank = CreateObject(tankModel, 1.0, 1.0, 1.0, true, true, false)
+    local bone1 = GetPedBoneIndex(cache.ped, 24818)
+    AttachEntityToEntity(currentGear.tank, cache.ped, bone1, -0.25, -0.25, 0.0, 180.0, 90.0, 0.0, true, true, false, false, 2, true)
+    
+    currentGear.mask = CreateObject(maskModel, 1.0, 1.0, 1.0, true, true, false)
+    local bone2 = GetPedBoneIndex(cache.ped, 12844)
+    AttachEntityToEntity(currentGear.mask, cache.ped, bone2, 0.0, 0.0, 0.0, 180.0, 90.0, 0.0, true, true, false, false, 2, true)
 end
 
 local function takeOffSuit()
@@ -51,12 +78,9 @@ local function takeOffSuit()
         exports.qbx_core:Notify(Lang:t("success.took_out"))
         TriggerServerEvent("InteractSound_SV:PlayOnSource", nil, 0.25)
     end
-
-    ClearPedTasks(cache.ped)
 end
 
-local function DrawText(text)
-    ClearPedTasks(cache.ped)
+local function drawText(text)
 	SetTextFont(4)
 	SetTextProportional(true)
 	SetTextScale(0.0, 0.45)
@@ -67,6 +91,36 @@ local function DrawText(text)
 	BeginTextCommandDisplayText("STRING")
 	AddTextComponentSubstringPlayerName(text)
     EndTextCommandDisplayText(0.45, 0.90)
+end
+
+local function startOxygenLevelDrawTextThread()
+    CreateThread(function()
+        while currentGear.enabled do
+            if IsPedSwimmingUnderWater(cache.ped) then
+                drawText(oxygenLevel..'⏱')
+            end
+            Wait(0)
+        end
+    end)
+end
+
+local function startOxygenLevelDecrementerThread()
+    CreateThread(function()
+        while currentGear.enabled do
+            if IsPedSwimmingUnderWater(cache.ped) and oxygenLevel > 0 then
+                oxygenLevel -= 1
+                if oxygenLevel % 10 == 0 and oxygenLevel ~= Config.OxygenLevel then
+                    TriggerServerEvent("InteractSound_SV:PlayOnSource", "breathdivingsuit", 0.25)
+                end
+                if oxygenLevel == 0 then
+                    disableScuba()
+                    TriggerServerEvent("InteractSound_SV:PlayOnSource", nil, 0.25)
+                    break
+                end
+            end
+            Wait(1000)
+        end
+    end)
 end
 
 local function putOnSuit()
@@ -92,47 +146,13 @@ local function putOnSuit()
         }
     }) then
         deleteGear()
-        local maskModel = `p_d_scuba_mask_s`
-        local tankModel = `p_s_scuba_tank_s`
-        lib.requestModel(maskModel)
-        lib.requestModel(tankModel)
-        currentGear.tank = CreateObject(tankModel, 1.0, 1.0, 1.0, true, true, false)
-        local bone1 = GetPedBoneIndex(cache.ped, 24818)
-        AttachEntityToEntity(currentGear.tank, cache.ped, bone1, -0.25, -0.25, 0.0, 180.0, 90.0, 0.0, true, true, false, false, 2, true)
-        currentGear.mask = CreateObject(maskModel, 1.0, 1.0, 1.0, true, true, false)
-        local bone2 = GetPedBoneIndex(cache.ped, 12844)
-        AttachEntityToEntity(currentGear.mask, cache.ped, bone2, 0.0, 0.0, 0.0, 180.0, 90.0, 0.0, true, true, false, false, 2, true)
-        SetEnableScuba(cache.ped, true)
-        SetPedMaxTimeUnderwater(cache.ped, 2000.00)
+        attachGear()
+        enableScuba()
         currentGear.enabled = true
         TriggerServerEvent("InteractSound_SV:PlayOnSource", "breathdivingsuit", 0.25)
-        CreateThread(function()
-            while currentGear.enabled do
-                if IsPedSwimmingUnderWater(cache.ped) and oxygenLevel > 0 then
-                    oxygenLevel -= 1
-                    if oxygenLevel % 10 == 0 and oxygenLevel ~= Config.OxygenLevel then
-                        TriggerServerEvent("InteractSound_SV:PlayOnSource", "breathdivingsuit", 0.25)
-                    elseif oxygenLevel == 0 then
-                        SetEnableScuba(cache.ped, false)
-                        SetPedMaxTimeUnderwater(cache.ped, 1.00)
-                        TriggerServerEvent("InteractSound_SV:PlayOnSource", nil, 0.25)
-                        break
-                    end
-                end
-                Wait(1000)
-            end
-        end)
-        CreateThread(function()
-            while currentGear.enabled do
-                if IsPedSwimmingUnderWater(cache.ped) then
-                    DrawText(oxygenLevel..'⏱')
-                end
-                Wait(0)
-            end
-        end)
+        startOxygenLevelDecrementerThread()
+        startOxygenLevelDrawTextThread()
     end
-
-    ClearPedTasks(cache.ped)
 end
 
 RegisterNetEvent('qbx_divegear:client:useGear', function()
