d72d119b73f6dc32b8c11d62bbe3f4c3924d785c
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-03-03 11:56:04 -0500

    refactor: update modules and locales

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,19 +5,21 @@ description 'items to help players breathe and move underwater'
 repository 'https://github.com/Qbox-project/qbx_divegear'
 version '1.0.0'
 
+ox_lib 'locale'
+
 shared_scripts {
     '@ox_lib/init.lua',
-    '@qbx_core/modules/utils.lua',
-    '@qbx_core/shared/locale.lua',
-    'locales/en.lua',
-    'locales/*.lua',
+    '@qbx_core/modules/lib.lua',
 }
 
 server_script 'server/main.lua'
 
 client_script 'client/main.lua'
 
-file 'config/client.lua'
+files {
+    'config/client.lua',
+    'locales/*.json',
+}
 
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
